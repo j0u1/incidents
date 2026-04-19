622e4d274d57c1754fa873961f3a24e23e283dd3
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-19 14:54:30 +0300

    feat: add auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import { Elysia } from "elysia";
+import { Context, Elysia } from "elysia";
+import { auth } from "./utils/auth";
+import { pingPong } from "./module/ping";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const betterAuthView = (context: Context) => {
+
+    if (["POST", "GET"].includes(context.request.method)) {
+        return auth.handler(context.request)
+    }
+    context.set.status = 405
+    return "Method Not Allowed"
+}
+
+const app = new Elysia()
+    .use(pingPong)
+    .use(cors({
+        origin: ["http://localhost:3001", "https://localhost:3001"],
+        credentials: true,
+    }))
+    .all("/api/auth/*", betterAuthView)
+    .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
