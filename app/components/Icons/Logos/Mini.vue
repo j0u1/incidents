ff2206416dde24b3fe3bc617a7f5e4ad8dce9c5a
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-21 15:43:52 +0300

    style(logo): change border radius

--- app/components/Icons/Logos/Mini.vue
+++ app/components/Icons/Logos/Mini.vue
@@ -16,13 +16,23 @@ const logoSize = computed(() => {
 </script>
 
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" :style="{ width: logoSize, height: logoSize }" fill="none" viewBox="0 0 48 48">
-        <path fill="#87c2fa" d="M24 18a6 6 0 1 1 0 12 6 6 0 0 1 0-12" />
+    <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        :style="{ width: logoSize, height: logoSize }"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
         <path
-            fill="#87c2fa"
+            d="M24 18C27.3137 18 30 20.6863 30 24C30 27.3137 27.3137 30 24 30C20.6863 30 18 27.3137 18 24C18 20.6863 20.6863 18 24 18Z"
+            fill="#87C2FA"
+        />
+        <path
             fill-rule="evenodd"
-            d="M24 0c13.255 0 24 10.745 24 24S37.255 48 24 48 0 37.255 0 24 10.745 0 24 0m0 9C15.716 9 9 15.716 9 24s6.716 15 15 15 15-6.716 15-15S32.284 9 24 9"
             clip-rule="evenodd"
+            d="M32 0C40.8366 0 48 7.16344 48 16V32C48 40.8366 40.8366 48 32 48H16C7.16344 48 2.5772e-07 40.8366 0 32V16C0 7.16344 7.16344 2.5772e-07 16 0H32ZM24 9C15.7157 9 9 15.7157 9 24C9 32.2843 15.7157 39 24 39C32.2843 39 39 32.2843 39 24C39 15.7157 32.2843 9 24 9Z"
+            fill="#87C2FA"
         />
     </svg>
 </template>
