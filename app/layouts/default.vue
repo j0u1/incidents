f7e6440c7dff5b6d50388f2946abb9de20722270
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-21 11:34:07 +0300

    style: change sign out transition

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -71,7 +71,7 @@ async function signOut() {
                     <button
                         @click="signOut"
                         type="button"
-                        class="text-sm text-light-gray border border-border rounded-lg flex items-center gap-1.5 w-full bg-bg hover:border-red hover:text-red duratiom-300 transition-300 cursor-pointer"
+                        class="text-sm text-light-gray border border-border rounded-lg flex items-center gap-1.5 w-full bg-bg hover:border-red hover:text-red duratiom-300 transition-all cursor-pointer"
                         :class="padding"
                     >
                         <LogOutIcon class="size-4.5" />
