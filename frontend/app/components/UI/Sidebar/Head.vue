21c40d2b53893d3834c190a6171582987df29819
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-30 11:01:23 +0300

    style(sidebar): change margin x

--- frontend/app/components/UI/Sidebar/Head.vue
+++ frontend/app/components/UI/Sidebar/Head.vue
@@ -12,10 +12,14 @@ const emit = defineEmits(["toggle"]);
 </script>
 
 <template>
-    <div class="relative flex justify-between items-center duration-300 transition-all">
+    <div
+        class="relative flex justify-between items-center duration-300 transition-all"
+    >
         <NuxtLink
             class="duration-300 transition-all active:scale-105"
-            :class="[isCollapsed ? 'mx-auto pt-6.5 pb-4' : 'mx-4 pt-6.75 pb-2.5']"
+            :class="[
+                isCollapsed ? 'mx-auto pt-6.5 pb-4' : 'mx-6 pt-6.75 pb-2.5',
+            ]"
             to="/"
         >
             <IconsLogosFull v-if="!isCollapsed" />
