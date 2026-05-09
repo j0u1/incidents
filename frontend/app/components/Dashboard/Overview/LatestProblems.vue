92030c0f41f320bf86a1331b1bad964950e69ecd
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-09 17:27:13 +0300

    refactor: remove card wrapper from problems

--- frontend/app/components/Dashboard/Overview/LatestProblems.vue
+++ frontend/app/components/Dashboard/Overview/LatestProblems.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <UICard class="min-h-67" title="Последние проблемы">
-        <DashboardProblems />
-    </UICard>
+  <DashboardProblems />
 </template>
