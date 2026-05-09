59a639726a647f94cf34a11e6b848f9f7876d982
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-09 13:24:03 +0300

    feat(stats): add icons

--- frontend/app/components/Dashboard/Stats.vue
+++ frontend/app/components/Dashboard/Stats.vue
@@ -1,32 +1,40 @@
 <script setup lang="ts">
+import { CheckIcon, Clock10Icon, TriangleAlertIcon } from '@lucide/vue';
+
 
 const { tickets, getTickets } = useTickets()
 await getTickets()
 const problemsSection = computed(() => [
     {
-        title: "Всего проблем",
+        title: "Нерешённых проблем",
         count: tickets.value.length,
         color: "red",
+        icon: TriangleAlertIcon,
     },
     {
         title: "В работе",
         count: 4,
         color: "orange",
+        icon: Clock10Icon
     },
     {
         title: "Всего решений",
         count: 12,
         color: "green",
+        icon: CheckIcon
     },
 ]);
 </script>
 
 <template>
     <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6 duration-300 transition-all">
-        <UICard v-for="ps in problemsSection" :key="ps.title">
+        <div v-for="ps in problemsSection" :key="ps.title" class="relative rounded-lg border border-border py-3 px-3.5 gap-2.5">
             <div class="absolute -top-1 -left-1 size-3.5 rounded-full" :style="{ backgroundColor: `var(--${ps.color})` }" />
-            <span class="text-5xl font-medium w-fit">{{ ps.count }}</span>
+            <div class="flex items-center gap-3.5">
+                <component :is="ps.icon" class="text-gray size-6.5" />
+                <span class="text-5xl font-medium w-fit">{{ ps.count }}</span>
+            </div>
             <span class="text-sm text-gray">{{ ps.title }}</span>
-        </UICard>
+        </div>
     </div>
 </template>
