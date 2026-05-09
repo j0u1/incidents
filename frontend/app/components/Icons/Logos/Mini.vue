73e948ec0272744182bf29fe56b1cd1acbb0d682
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-09 18:30:31 +0300

    style(logo): change fill color

--- frontend/app/components/Icons/Logos/Mini.vue
+++ frontend/app/components/Icons/Logos/Mini.vue
@@ -23,9 +23,9 @@ const logoSize = computed(() => {
     fill="none"
     :style="{ width: logoSize, height: logoSize }"
   >
-    <path fill="#87c2fa" d="M13 9.75a3.25 3.25 0 1 1 0 6.5 3.25 3.25 0 0 1 0-6.5" />
+    <path fill="#60a5fa" d="M13 9.75a3.25 3.25 0 1 1 0 6.5 3.25 3.25 0 0 1 0-6.5" />
     <path
-      fill="#87c2fa"
+      fill="#60a5fa"
       fill-rule="evenodd"
       d="M17.333 0A8.667 8.667 0 0 1 26 8.667v8.666A8.667 8.667 0 0 1 17.333 26H8.667A8.667 8.667 0 0 1 0 17.333V8.667A8.667 8.667 0 0 1 8.667 0zM13 4.875a8.125 8.125 0 1 0 0 16.25 8.125 8.125 0 0 0 0-16.25"
       clip-rule="evenodd"
