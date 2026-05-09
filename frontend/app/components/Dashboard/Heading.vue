5e1b90178439b739e571f75b90df00ee7ed31d5b
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-09 12:44:43 +0300

    style(sidebar): change heading height

--- frontend/app/components/Dashboard/Heading.vue
+++ frontend/app/components/Dashboard/Heading.vue
@@ -31,7 +31,7 @@ withDefaults(
 </script>
 
 <template>
-    <div class="flex items-end justify-between px-5 h-16.75">
+    <div class="flex items-end justify-between px-5 h-18.25">
         <h1 class="text-3xl font-medium">{{ title }}</h1>
         <!-- <div
             class="flex items-center gap-2 text-sm text-light-gray rounded-xl whitespace-nowrap select-none border border-border"
