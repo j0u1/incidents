116c0b224831e2e571a44e1100b8a65253049bfb
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-11 20:36:35 +0300

    fix(index.vue): remove GitHub sign-in function and clean up template structure

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,61 +1,34 @@
 <script setup lang="ts">
 import { NuxtLink } from "#components";
 import { LayoutIcon, LogInIcon } from "@lucide/vue";
-import { padding } from "~/data/dynamicStyles";
 
 const authClient = useAuth();
 const session = authClient.useSession();
-
-async function signInWithGithub() {
-    const callbackURL = `${window.location.origin}/dashboard`;
-    const { data, error } = await authClient.signIn.social({
-        provider: "github",
-        callbackURL,
-        disableRedirect: true,
-    });
-
-    if (error) {
-        console.error("GitHub sign-in failed", error);
-        return;
-    }
-
-    if (data?.url) {
-        window.location.href = data.url;
-    }
-}
 </script>
 
 <template>
-    <main
-        class="flex flex-col gap-16 items-center max-h-[87.5dvh] overflow-hidden border-b border-border"
+  <main
+    class="flex flex-col gap-16 items-center max-h-[87.5dvh] overflow-hidden border-b border-border"
+  >
+    <h1 class="text-6xl font-medium flex flex-col gap-2 text-center pt-18">
+      <span>Проблемы случаются,</span>
+      <span class="text-primary">Вы решите их быстрее</span>
+    </h1>
+    <UIButtonBase
+      v-if="!session.data?.session"
+      :as="NuxtLink"
+      href="/login"
+      variant="filled"
+      :icon="LogInIcon"
     >
-        <h1 class="text-6xl font-medium flex flex-col gap-2 text-center pt-18">
-            <span>Проблемы случаются,</span>
-            <span class="text-primary">Вы решите их быстрее</span>
-        </h1>
-        <!-- <UIButton v-if="!session.data?.session" variant="login" long /> -->
-        <UIButtonBase
-            v-if="!session.data?.session"
-            :as="NuxtLink"
-            href="/login"
-            variant="filled"
-            :icon="LogInIcon"
-        >
-            Войти в систему
-        </UIButtonBase>
-        <UIButtonBase
-            v-else
-            :as="NuxtLink"
-            href="/dashboard"
-            variant="filled"
-            :icon="LayoutIcon"
-        >
-            Перейти в Обзор
-        </UIButtonBase>
-        <NuxtImg
-            class="max-w-11/12 mx-auto rounded-2xl outline outline-border w-full"
-            src="/images/preview.webp"
-        />
-    </main>
-    <!-- <section class="flex flex-col px-20"></section> -->
+      Войти в систему
+    </UIButtonBase>
+    <UIButtonBase v-else :as="NuxtLink" href="/dashboard" variant="filled" :icon="LayoutIcon">
+      Перейти в Обзор
+    </UIButtonBase>
+    <NuxtImg
+      class="max-w-11/12 mx-auto rounded-2xl outline outline-border w-full"
+      src="/images/preview.webp"
+    />
+  </main>
 </template>
