bf58959d960c987f6e3796e4df836e90a21a3d35
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-11 18:50:13 +0300

    fix(AuthForm.vue, Input/index.vue, login.vue): refactor authentication components for improved structure and functionality

--- frontend/app/components/UI/AuthForm.vue
+++ frontend/app/components/UI/AuthForm.vue
@@ -1,111 +1,143 @@
 <script setup lang="ts">
-import type { LucideIcon } from '@lucide/vue';
-import { padding } from '~/data/dynamicStyles';
+import { ArrowRightIcon, LockIcon, LogInIcon, MailIcon, type LucideIcon } from "@lucide/vue";
+import GitHub from "~/components/Icons/GitHub.vue";
 
-const { signIn } = useAuth()
+const { signIn } = useAuth();
+const authError = ref("");
 
 async function signInWithGithub() {
-    const callbackURL = `${window.location.origin}/dashboard`;
-    const { data, error } = await signIn.social({
-        provider: "github",
-        callbackURL,
-        disableRedirect: true,
-    });
+  const callbackURL = `${window.location.origin}/dashboard`;
 
-    if (error) {
-        console.error("GitHub sign-in failed", error);
-        return;
-    }
+  const { data, error } = await signIn.social({
+    provider: "github",
+    callbackURL,
+    disableRedirect: true,
+  });
 
-    if (data?.url) {
-        window.location.href = data.url;
-    }
+  if (error) {
+    console.error("GitHub sign-in failed", error);
+    return;
+  }
+
+  if (data?.url) {
+    window.location.href = data.url;
+  }
 }
 
 interface Inputs {
-    key: string;
-    label: string;
-    type: string;
-    placeholder: string;
-    icon: LucideIcon;
+  key: string;
+  label: string;
+  type: string;
+  placeholder: string;
+  icon: LucideIcon;
 }
 
-defineProps<{
+const props = withDefaults(
+  defineProps<{
     isLogin: boolean;
-    inputs: Inputs[];
-}>();
+    inputs?: Inputs[];
+  }>(),
+  {
+    inputs: () => [
+      {
+        key: "email",
+        label: "Почта",
+        type: "email",
+        placeholder: "Введите ваш email",
+        icon: MailIcon,
+      },
+      {
+        key: "password",
+        label: "Пароль",
+        type: "password",
+        placeholder: "Введите ваш пароль",
+        icon: LockIcon,
+      },
+    ],
+  },
+);
+
+type Form = {
+  email: string;
+  password: string;
+};
+
+const form = reactive<Form>({
+  email: "",
+  password: "",
+});
+
+async function handleSignIn() {
+  const { error } = await signIn.email({
+    email: form.email.trim(),
+    password: form.password,
+    callbackURL: "/",
+  });
+
+  if (error) {
+    if (error.code === "INVALID_EMAIL_OR_PASSWORD") {
+      authError.value = "Неверный логин или пароль";
+    } else if (error.message === "Invalid email") {
+      authError.value = "Не существует такой почты";
+    } else {
+      authError.value = "Ошибка входа: " + error.message;
+    }
+    return;
+  }
+  navigateTo("/dashboard");
+}
 </script>
 
 <template>
-    <section class="mx-auto max-w-md h-dvh flex flex-col gap-6 justify-center">
-        <div class="mx-auto flex gap-2 items-center">
-            <NuxtLink to="/">
-                <IconsLogosFull />
-            </NuxtLink>
-            <ArrowRightIcon class="size-4.5 text-gray" />
-            <p class="text-light-gray">
-                {{ isLogin ? "Вход в систему" : "Регистрация" }}
-            </p>
+  <section class="mx-auto max-w-md h-dvh flex flex-col gap-6 justify-center">
+    <div class="mx-auto flex gap-2 items-center">
+      <NuxtLink to="/">
+        <IconsLogosFull />
+      </NuxtLink>
+      <ArrowRightIcon class="size-4.5 text-gray" />
+      <p class="text-light-gray">
+        {{ isLogin ? "Вход в систему" : "Регистрация" }}
+      </p>
+    </div>
+    <div class="rounded-xl outline outline-border p-6 flex flex-col gap-6 bg-bg">
+      <form @submit.prevent="handleSignIn" class="flex flex-col gap-4 w-full">
+        <UIInput
+          v-for="input in inputs"
+          :key="input.key"
+          v-model="form[input.key as keyof Form]"
+          :label="input.label"
+          :type="input.type"
+          :icon="input.icon"
+          :placeholder="input.placeholder"
+        />
+        <hr class="border-border w-full" />
+        <div class="grid grid-cols-2 gap-4">
+          <UIButtonBase type="submit" :icon="LogInIcon">
+            {{ isLogin ? "Войти" : "Зарегистрироваться" }}
+          </UIButtonBase>
+          <UIButtonBase @click="signInWithGithub" :icon="GitHub" class="w-full">
+            GitHub
+          </UIButtonBase>
         </div>
-        <div
-            class="rounded-xl outline outline-border p-6 flex flex-col gap-6 bg-bg"
-        >
-            <form @submit.prevent="signIn" class="flex flex-col gap-4 w-full">
-                <UIInput
-                    v-for="input in inputs"
-                    :key="input.key"
-                    v-model="form[input.key]"
-                    :label="input.label"
-                    :type="input.type"
-                    :icon="input.icon"
-                    :placeholder="input.placeholder"
-                />
-                <hr class="border-border w-full" />
-                <div class="flex items-center gap-4">
-                    <button
-                        type="submit"
-                        class="bg-secondary rounded-lg text-light-gray flex items-center gap-2.5 justify-center w-full cursor-pointer"
-                        :class="padding"
-                    >
-                        <LogInIcon class="size-4.5" />
-                        Войти
-                    </button>
-                    <button
-                        v-if="isLogin"
-                        type="button"
-                        class="bg-secondary rounded-lg text-light-gray fill-light-gray flex items-center gap-2.5 justify-center w-full cursor-pointer"
-                        :class="padding"
-                        @click="signInWithGithub"
-                    >
-                        <IconsGitHub class="size-4.5" />
-                        GitHub
-                    </button>
-                </div>
-            </form>
-        </div>
+      </form>
+    </div>
 
-        <div class="relative text-center">
-            <NuxtLink
-                class="text-sm text-gray duration-300 transition-all mx-auto group/reg"
-                to="/register"
-            >
-                Нет аккаунта?
-                <span
-                    class="text-light-gray group-hover/reg:text-primary duration-300 transition-all"
-                >
-                    Зарегистрироваться
-                </span>
-            </NuxtLink>
-            <p
-                class="absolute -bottom-6 inset-x-0 text-sm duration-300 origin-top transition-all"
-                :class="[
-                    authError
-                        ? 'text-red-500 scale-100'
-                        : 'text-light-gray scale-0',
-                ]"
-            >
-                {{ authError }}
-            </p>
-        </div>
-    </section>
+    <div class="relative text-center">
+      <NuxtLink
+        class="text-sm text-gray duration-300 transition-all mx-auto group/reg"
+        to="/register"
+      >
+        Нет аккаунта?
+        <span class="text-light-gray group-hover/reg:text-primary duration-300 transition-all">
+          Зарегистрироваться
+        </span>
+      </NuxtLink>
+      <p
+        class="absolute -bottom-6 inset-x-0 text-sm duration-300 origin-top transition-all"
+        :class="[authError ? 'text-red-500 scale-100' : 'text-light-gray scale-0']"
+      >
+        {{ authError }}
+      </p>
+    </div>
+  </section>
 </template>
--- frontend/app/components/UI/Input/index.vue
+++ frontend/app/components/UI/Input/index.vue
@@ -1,44 +1,47 @@
 <script setup lang="ts">
 import { padding } from "~/data/dynamicStyles";
-import type { LucideIcon } from "@lucide/vue";
+import { type LucideIcon } from "@lucide/vue";
+
+const showPassword = ref(false);
 
 defineProps<{
-    label: string;
-    additionalText?: string;
-    placeholder: string;
-    icon?: LucideIcon;
-    type: string;
-    modelValue: string | undefined;
+  label: string;
+  additionalText?: string;
+  placeholder: string;
+  icon?: LucideIcon;
+  type: string;
+  modelValue: string | undefined;
 }>();
 
 const emit = defineEmits<{
-    (e: "update:modelValue", value: string): void;
+  (e: "update:modelValue", value: string): void;
 }>();
 </script>
 
 <template>
-    <div class="flex flex-col gap-2">
-        <UIInputLabel :label="label" :additionalText="additionalText" />
-        <div
-            class="relative outline outline-border rounded-lg flex items-center text-gray focus-within:text-primary duration-300 transition-all focus-within:outline-primary"
-        >
-            <component
-                :is="icon"
-                class="absolute left-4 size-4.5 pointer-events-none"
-            />
-            <input
-                class="pl-12 w-full bg-transparent outline-none text-light-gray"
-                :class="padding"
-                :type="type"
-                :placeholder="placeholder"
-                :value="modelValue"
-                @input="
-                    emit(
-                        'update:modelValue',
-                        ($event.target as HTMLInputElement).value,
-                    )
-                "
-            />
-        </div>
+  <div class="flex flex-col gap-2">
+    <div class="flex items-center justify-between">
+      <UIInputLabel :label="label" :additionalText="additionalText" />
+      <div
+        v-if="type === 'password'"
+        @click="showPassword = !showPassword"
+        class="flex items-center gap-1.5 text-gray text-sm hover:text-primary duration-300 transition-all cursor-pointer"
+      >
+        {{ !showPassword ? "Показать пароль" : "Скрыть пароль" }}
+      </div>
     </div>
+    <div
+      class="relative outline outline-border rounded-lg flex items-center text-gray focus-within:text-primary duration-300 transition-all focus-within:outline-primary"
+    >
+      <component :is="icon" class="absolute left-4 size-4.5 pointer-events-none" />
+      <input
+        class="pl-12 w-full bg-transparent outline-none text-light-gray"
+        :class="padding"
+        :type="type === 'password' ? (showPassword ? 'text' : 'password') : type"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      />
+    </div>
+  </div>
 </template>
