b09f32e6a51204ff8d148738cebadb4d5ed9ba96
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-13 13:29:32 +0300

    refactor(features): simplify features export and remove unnecessary variable

--- frontend/app/config/features.ts
+++ frontend/app/config/features.ts
@@ -1,12 +1,8 @@
-const _features = {
+export const features = {
   auth: true, // ни к чему не привязано
   problems: true,
   users: false,
   settings: false,
   statuses: true, // от статусов зависит статистика по статусам
+  stats: false, 
 };
-
-export const features = {
-  ..._features,
-  stats: _features.statuses, 
-}
