8658d54726651f1f5225de271f2a2366dd34eee1
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-13 13:40:40 +0300

    fix(app): update layout for better visibility on larger screens

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,11 @@
 <template>
-    <NuxtRouteAnnouncer class="absolute" />
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
+  <NuxtRouteAnnouncer class="absolute" />
+  <NuxtLayout class="hidden lg:flex">
+    <NuxtPage />
+  </NuxtLayout>
+  <div
+    class="flex items-center justify-center h-screen lg:hidden duration-300 transition-all text-gray"
+  >
+    Доступная только компьютерная версия
+  </div>
 </template>
