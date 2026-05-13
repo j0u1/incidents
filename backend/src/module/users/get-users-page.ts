efc6627318f8f3163d9cf0b1d4c087677271588f
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-13 11:19:45 +0300

    fix(get-users-page): remove unused getUsersPage function and clean up code

--- backend/src/module/users/get-users-page.ts
+++
@@ -1,6 +0,0 @@
-import Elysia from "elysia";
-
-export const getUsersPage = new Elysia()
-    .get("/users/:a/:b", async ({status, params:{a, b}, query: { c }}) => {
-        return status(201, Number(a) + Number(b) + Number(c))
-    })
