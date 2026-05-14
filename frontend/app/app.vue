f7f377366cacb3ea927ada416e5888434791421c
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-05-14 13:02:03 +0300

    fix(app): update route condition for mobile view and adjust layout styling

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
 <template>
   <NuxtRouteAnnouncer class="absolute" />
   <NuxtLayout class="hidden lg:flex">
     <NuxtPage />
   </NuxtLayout>
   <div
+    v-if="route.name !== 'index'"
     class="flex items-center justify-center h-screen lg:hidden duration-300 transition-all text-gray"
   >
     Доступная только компьютерная версия
