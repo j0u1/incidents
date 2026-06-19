ded691de954bc11b994bd4fbeac7a93a52a78c33
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-19 14:59:18 +0300

    feat(Dashboard): add grid toggle and search input to overview component

---
+++ frontend/app/components/Dashboard/Overview/Head.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { SearchIcon } from "@lucide/vue";
+
+const search = ref("");
+
+defineProps<{
+  isGrid: boolean;
+}>();
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <UIInput
+      :icon="SearchIcon"
+      placeholder="Поиск по инцидентам"
+      type="search"
+      :modelValue="search"
+      class="w-full max-w-141.5"
+    />
+    <button
+      @click="isGrid = !isGrid"
+      type="button"
+      class="flex items-center gap-2.5 duration-300 transition-all cursor-pointer w-fit active:scale-95"
+      :class="isGrid ? 'text-primary' : 'text-gray hover:text-primary'"
+    >
+      <Grid2X2Icon class="size-4.5 shrink-0" />
+      Сетка
+    </button>
+  </div>
+</template>
--- frontend/app/components/Dashboard/Overview/index.vue
+++ frontend/app/components/Dashboard/Overview/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { features } from "~/config/features";
+
+const isGrid = ref(false);
 </script>
 
 <template>
+  <DashboardOverviewHead :is-grid="isGrid" />
   <div class="flex flex-col xl:flex-row duration-300 transition-all gap-6">
-    <DashboardOverviewLatestProblems isShort v-if="features.problems" />
+    <DashboardProblems :is-grid="isGrid" is-short v-if="features.problems" />
     <DashboardOverviewStats v-if="features.stats" />
   </div>
 </template>
