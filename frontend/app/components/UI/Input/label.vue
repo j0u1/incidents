7d382054abfd05d5a890a008c7374f7e386ee41f
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-19 15:11:38 +0300

    fix(Input): add required prop and update label for better accessibility

--- frontend/app/components/UI/Input/label.vue
+++ frontend/app/components/UI/Input/label.vue
@@ -2,12 +2,17 @@
 defineProps<{
   label: string;
   additionalText?: string;
+  required?: boolean;
 }>();
 </script>
 
 <template>
-  <label>
+  <label
+    :class="required && 'cursor-help'"
+    :title="required ? 'Обязательное для заполнения поле' : undefined"
+  >
     {{ label }}
+    <span v-if="required" class="text-red">*</span>
     <span v-if="additionalText" class="text-gray text-sm">
       {{ additionalText }}
     </span>
