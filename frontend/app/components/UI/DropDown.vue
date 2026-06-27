ae8b4557b8f61bf7d0db68381f82c96494fe5346
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-27 22:51:14 +0300

    fix(DropDown): adjust dropdown positioning and opacity for smoother transitions

--- frontend/app/components/UI/DropDown.vue
+++ frontend/app/components/UI/DropDown.vue
@@ -18,8 +18,7 @@ const props = defineProps<{
     ref="dropDownRef"
     class="absolute border border-border rounded-xl flex flex-col items-center gap-2.5 duration-200 transition-all bg-secondary origin-top-right w-56 right-0 p-2.5"
     :class="[
-      props.session.data ? 'opacity-100' : 'opacity-0',
-      isOpen ? 'top-12 scale-100 opacity-100' : 'top-8 scale-0 opacity-0',
+      isOpen ? 'top-12 scale-100 opacity-100' : 'top-7.5 scale-30 opacity-0',
     ]"
   >
     <UIPages dropDown />
